f782df43ac1353bd27bc4cd4f258adf510687030
katrina <katrina.nov13@gmail.com>
2020-03-18 12:14:34 +0300

    first lesson: live search

---
+++ script.js
@@ -0,0 +1,57 @@
+const formSearch = document.querySelector('.form-search'),
+      inputCitiesFrom = document.querySelector('.input__cities-from'),
+      dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+      inputCitiesTo = document.querySelector('.input__cities-to'),
+      dropdownCitiesTo = document.querySelector('.dropdown__cities-to');
+
+const city = ['Москва', 'Санкт-петербург', 'Минск', 'Караганда', 'Челябинск', 'Керч', 'Волгоград', 'Самара', 'Одесса', 'Калининград', 'Вроцлав', 'Ростов-на-Дону', 'Варшава', 'Рига', 'Киев'];
+
+
+// function expression. Вызывается только после объявления. const get = function() {}
+
+const showCity = (input, list) => {
+
+  list.textContent = '';
+
+  if (input.value !== '') {  
+    const filterCity = city.filter((item) => {
+      const fixItem = item.toLowerCase();
+      return fixItem.includes(input.value.toLowerCase());
+    });
+
+    filterCity.forEach((item) => {
+      const li = document.createElement('li');
+      li.classList.add('dropdown__city');
+      li.textContent = item;
+      list.append(li);
+    });
+  }
+};
+
+inputCitiesFrom.addEventListener('input', () => {
+  showCity(inputCitiesFrom, dropdownCitiesFrom);
+});
+
+inputCitiesTo.addEventListener('input', () => {
+  showCity(inputCitiesTo, dropdownCitiesTo);
+});
+
+dropdownCitiesFrom.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === 'li') {
+    inputCitiesFrom.value = target.textContent;
+    dropdownCitiesFrom.textContent = '';
+  }
+});
+
+dropdownCitiesTo.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === 'li') {
+    inputCitiesTo.value = target.textContent;
+    dropdownCitiesTo.textContent = '';
+  }
+});
+
+
+
+
