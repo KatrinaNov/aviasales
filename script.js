c27a89e303a55241cc80272daf35888e6abb68e7
katrina <katrina.nov13@gmail.com>
2020-03-19 16:40:08 +0300

    second lesson

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ const formSearch = document.querySelector('.form-search'),
       inputCitiesTo = document.querySelector('.input__cities-to'),
       dropdownCitiesTo = document.querySelector('.dropdown__cities-to');
 
-const city = ['Москва', 'Санкт-петербург', 'Минск', 'Караганда', 'Челябинск', 'Керч', 'Волгоград', 'Самара', 'Одесса', 'Калининград', 'Вроцлав', 'Ростов-на-Дону', 'Варшава', 'Рига', 'Киев'];
+const citiesApi = 'dataBase/cities.json',
+      proxy = 'https://cors-anywhere.herokuapp.com/',
+      API_KEY = '0d684f68cc459a8d976b016cc1431ff5',
+      calendar = 'http://min-prices.aviasales.ru/calendar_preload';
+      
+let city = [];
 
+// получаем данные с сервера
+const getData = (url, callback) => {
+  const request = new XMLHttpRequest(); // объект запроса  
+  request.open('GET', url); // получать данные и отправлять url
+
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState !==4) return;
+    if (request.status === 200) {
+      callback(request.response);
+    } else {
+      console.error(request.status);
+    }    
+  });
+
+  request.send();
+};
 
 // function expression. Вызывается только после объявления. const get = function() {}
-
 // функция поиска нужного города, который содержит в названии введенные символы в инпут.  и вывода списка с подсказками
 const showCity = (input, list) => {
 
@@ -16,16 +36,24 @@ const showCity = (input, list) => {
 
   if (input.value !== '') {  
     const filterCity = city.filter((item) => {
-      const fixItem = item.toLowerCase();
+      const fixItem = item.name.toLowerCase();
       return fixItem.includes(input.value.toLowerCase());
     });
 
     filterCity.forEach((item) => {
       const li = document.createElement('li');
       li.classList.add('dropdown__city');
-      li.textContent = item;
+      li.textContent = item.name;
       list.append(li);
     });
+  }
+};
+
+const selectCity = (event, input, list) => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === 'li') {
+    input.value = target.textContent;
+    list.textContent = '';
   }
 };
 
@@ -41,21 +69,18 @@ inputCitiesTo.addEventListener('input', () => {
 
 // при клике на список с подсказками выводится нужное название в инпут
 dropdownCitiesFrom.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target.tagName.toLowerCase() === 'li') {
-    inputCitiesFrom.value = target.textContent;
-    dropdownCitiesFrom.textContent = '';
-  }
+  selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
 });
 
 dropdownCitiesTo.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target.tagName.toLowerCase() === 'li') {
-    inputCitiesTo.value = target.textContent;
-    dropdownCitiesTo.textContent = '';
-  }
+  selectCity(event, inputCitiesTo, dropdownCitiesTo);
 });
 
+// вызовы функций
+getData(citiesApi, (data) => city = JSON.parse(data).filter(item => item.name));
 
-
-
+// getData(citiesApi, (data) => {
+//   city = JSON.parse(data).filter((item) => {
+//     return item.name;
+//   });   
+// });
